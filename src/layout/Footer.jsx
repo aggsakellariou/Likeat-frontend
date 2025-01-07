e0be913218a753be681383af49ab1d3e2e5aa196
asak <aggelossakell@gmail.com>
2025-01-07 12:03:24 +0200

    Initial commit

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+  return (
+    <footer className="footer">
+      <div className="footer-content">
+        <p>© 2024 Copyright: LikEat</p>
+      </div>
+    </footer>
+  );
+}
